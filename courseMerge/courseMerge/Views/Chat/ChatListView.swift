990b7167e46851376ed29d4759d9d9de09308f44
iyungui <rsdbddml@gmail.com>
2024-06-10 21:55:10 +0900

    ChatListView

--- courseMerge/courseMerge/Views/Chat/ChatListView.swift
+++ courseMerge/courseMerge/Views/Chat/ChatListView.swift
@@ -7,14 +7,107 @@
 
 import SwiftUI
 
+struct ChatList: Identifiable {
+    let id = UUID()
+    let name: String
+    let participants: String
+}
+
+extension ChatList {
+    static var example: [ChatList] = [
+        ChatList(name: "동두천 파티", participants: "별빛 여우 외 7명"),
+        ChatList(name: "은평구 파티", participants: "무지개코끼리 외 2명"),
+        ChatList(name: "제주도 파티", participants: "별빛 여우 외 7명")
+    ]
+}
+
 struct ChatListView: View {
+    @State private var showNotification = false
+    @State private var exampleChatList = ChatList.example
+    @State private var isShowAlert: Bool = true
+    
     var body: some View {
-        VStack {
-            Text("ChatListView")
+        NavigationStack {
+            ZStack {
+                List {
+                    ForEach(exampleChatList) { item in
+                        
+                        NavigationLink(destination: ChatView()) {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                
+                                Text(item.participants)
+                                    .font(.subheadline)
+                                    .foregroundStyle(Color("LabelsSecondary"))
+                            }
+                        }
+                    }
+                    .onDelete(perform: { indexSet in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    })
+                    .onMove(perform: { indices, newOffset in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    })
+                }
+                .navigationTitle("채팅")
+                .toolbar {
+                    EditButton()
+                }
+                
+                VStack {
+                    // TODO: 앱 실행 후 한번만 나타나도록 하기!
+                    if showNotification {
+                        Text("채팅을 통해 파티원과 일정을 맞춰보세요!")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .transition(.move(edge: .top).combined(with: .opacity))
+                            .animation(.easeInOut(duration: 0.5), value: showNotification)
+                    }
+                    Spacer()
+                }
+            }
+            .onAppear {
+                showNotificationWithDelay()
+            }
+            .alert("알림", isPresented: $isShowAlert) {
+                Button("지금 안해요", role: .cancel) {
+                    isShowAlert = false
+                }
+                Button {
+                    
+                } label: {
+                    Text("추가")
+                }
+            } message: {
+                Text("현재 참여중인 파티가 없습니다.\n파티를 추가하시겠어요?")
+            }
+        }
+    }
+
+    private func showNotificationWithDelay() {
+        withAnimation {
+            showNotification = true
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            withAnimation {
+                showNotification = false
+            }
         }
     }
 }
 
 #Preview {
     ChatListView()
+}
+
+
+struct ChatView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
 }
