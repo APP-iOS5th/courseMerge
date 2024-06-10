4d488c4b87162a84e1862de82282ced635dd8e0e
Shiene Park <logintolookupsome@gmail.com>
2024-06-10 13:56:51 +0900

    NavigationLink

---
+++ courseMerge/courseMerge/Views/Setting/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  CourseMerge
+//
+//  Created by mini on 6/10/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text("Edit Profile")
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
--- courseMerge/courseMerge/Views/Setting/SettingVIew.swift
+++ courseMerge/courseMerge/Views/Setting/SettingVIew.swift
@@ -9,26 +9,92 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("프로파일 수정")
-                Text("차단한 사용자 관리")
-            } header: {
-                Text("일반")
-            }
-            Section {
-                Text("개인정보 처리방침")
-                Text("서비스 이용약관")
-                Text("개발자 정보")
-                Text("신고하기")
-            } header: {
-                Text("앱 정보")
-            }
-            Section {
-                Text("로그아웃")
-                Text("회원탈퇴")
-            } header: {
-                Text("계정")
+        NavigationStack {
+            Form {
+                Section {
+                    NavigationLink {
+                        EditProfileView()
+                    } label: {
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("프로파일 수정")
+                    }
+                    NavigationLink {
+//                        BlockedContactsView()
+                    } label: {
+                        Image(systemName: "person.slash")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("차단한 사용자 관리")
+                    }
+                } header: {
+                    Text("일반")
+                }
+                Section {
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "shield.lefthalf.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("개인정보 처리방침")
+                    }
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "doc.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("서비스 이용약관")
+                    }
+                   
+                    NavigationLink {
+                        
+                    } label: {
+                        Image(systemName: "hammer.fill")
+                        Text("개발자 정보")
+                    }
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "exclamationmark.bubble.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("신고하기")
+                    }
+                    
+                } header: {
+                    Text("앱 정보")
+                }
+                Section {
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "rectangle.portrait.and.arrow.right.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("로그아웃")
+                    }
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "person.crop.circle.fill.badge.xmark")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 28)
+                        Text("회원탈퇴")
+                    }
+                    
+                } header: {
+                    Text("계정")
+                }
             }
         }
     }
