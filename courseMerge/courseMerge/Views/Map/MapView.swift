f86e8d1c15fc30832bd8371321c7c6da16b6eb00
DopeKS <kyusang.dev@gmail.com>
2024-06-10 21:59:01 +0900

    Code Modularization 작업

--- courseMerge/courseMerge/Views/Map/MapView.swift
+++ courseMerge/courseMerge/Views/Map/MapView.swift
@@ -23,105 +23,110 @@ struct MapView: View {
                 Spacer()
             }
             
-            VStack {
-                Spacer()
-                HStack {
-                    Button(action: {
-                        print("현재위치버튼 클릭")
-                    }) {
-                        Image(systemName: "location.circle.fill")
-                            .resizable()
-                            .frame(width: 50, height: 50)
-                            .foregroundColor(.blue)
-                            .background(Color.white)
-                            .clipShape(Circle())
-                            .shadow(radius: 10)
-                    }
-                    .padding(.bottom, 50)
-                    .padding(.leading, 20)
-                    Spacer()
-                }
-            }
+            CurrentLocationButton()
         }
     }
 }
 
 struct HeaderView: View {
-    @State private var selectedDate = Date()
-    @State private var isExpanded = false
-    @State private var iconViewHeight: CGFloat = 0
-    @State private var expanded = false
-    @State private var showingActionSheet = false
-    @State private var activatedPartyName: String = "제주도 파티"
-    
     var body: some View {
         VStack {
             HStack {
-                Button {
-                    self.showingActionSheet = true
-                } label: {
-                    HStack {
-                        Image(systemName: "line.3.horizontal")
-                            .foregroundColor(.white)
-                        Text(activatedPartyName)
-                            .foregroundColor(.white)
-                    }
-                    .padding()
-                    .frame(width: 130, height: 34)
-                    .font(.system(size: 15))
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    .actionSheet(isPresented: $showingActionSheet) {
-                        ActionSheet(
-                            title: Text("파티를 선택해주세요"),
-                            message: nil,
-                            // TODO: 버튼 텍스트에 fontWeight 안들어감 ㅂㄷㅂㄷ
-                            buttons: [
-                                .default(Text("제주도 파티").fontWeight(.bold), action: {
-                                    self.activatedPartyName = "제주도 파티"
-                                }),
-                                .default(Text("은평구 파티").fontWeight(.regular), action: {
-                                    self.activatedPartyName = "은평구 파티"
-                                }),
-                                .default(Text("동두천 파티").fontWeight(.regular), action: {
-                                    self.activatedPartyName = "동두천 파티"
-                                }),
-                                .cancel(Text("Cancel"))
-                            ]
-                        )
-                    }
-                }
-                
-                DatePicker("", selection: $selectedDate, displayedComponents: .date)
-                    .datePickerStyle(CompactDatePickerStyle())
-                    .labelsHidden()
-                    .padding()
-                    .frame(width: 124, height: 34)
-                    .background(Color.clear)
-                    .cornerRadius(10)
-                
-                Button {
-                    // TODO: 검색화면 연결
-                } label: {
-                    Image(systemName: "magnifyingglass")
-                        .padding()
-                }
-                .frame(width: 24.64, height: 22)
+                PartySelectionButton()
+                PartyDateSelectionPicker()
+                PlaceSearchButton()
             }
             .frame(maxWidth: .infinity, alignment: .trailing)
             .frame(height: 48)
             
-            Text("지도")
-                .font(.largeTitle)
-                .bold()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .frame(height: 52)
-            
-            Divider()
+            viewTitleText()
         }
         .padding(.horizontal)
         .background(Color.white)
         
+        MemberCustomDisclosureGroup()
+    }
+}
+
+
+
+/// 파티를 선택할 수 있는 버튼(actionSheet)
+struct PartySelectionButton: View {
+    @State private var showingActionSheet = false
+    @State private var activatedPartyName: String = "제주도 파티"
+    
+    var body: some View {
+        Button {
+            self.showingActionSheet = true
+        } label: {
+            HStack {
+                Image(systemName: "line.3.horizontal")
+                    .foregroundColor(.white)
+                Text(activatedPartyName)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .frame(width: 130, height: 34)
+            .font(.system(size: 15))
+            .background(Color.blue)
+            .cornerRadius(20)
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(
+                    title: Text("파티를 선택해주세요"),
+                    message: nil,
+                    // TODO: 버튼 텍스트에 fontWeight 안들어감 ㅂㄷㅂㄷ
+                    buttons: [
+                        .default(Text("제주도 파티").fontWeight(.bold), action: {
+                            self.activatedPartyName = "제주도 파티"
+                        }),
+                        .default(Text("은평구 파티").fontWeight(.regular), action: {
+                            self.activatedPartyName = "은평구 파티"
+                        }),
+                        .default(Text("동두천 파티").fontWeight(.regular), action: {
+                            self.activatedPartyName = "동두천 파티"
+                        }),
+                        .cancel(Text("Cancel"))
+                    ]
+                )
+            }
+        }
+    }
+}
+
+/// 파티 구성 기간 중 특정 날짜에 대한 구성원들의 코스를 볼 수 있게 날짜를 선택하는 피커
+struct PartyDateSelectionPicker: View {
+    @State private var selectedDate = Date()
+    
+    var body: some View {
+        DatePicker("", selection: $selectedDate, displayedComponents: .date)
+            .datePickerStyle(CompactDatePickerStyle())
+            .labelsHidden()
+            .padding()
+            .frame(width: 124, height: 34)
+            .background(Color.clear)
+            .cornerRadius(10)
+    }
+}
+
+/// 장소검색버튼
+struct PlaceSearchButton: View {
+    var body: some View {
+        Button {
+            // TODO: 검색화면 연결
+        } label: {
+            Image(systemName: "magnifyingglass")
+                .padding()
+        }
+        .frame(width: 24.64, height: 22)
+    }
+}
+
+/// 선택된 파티에 따라 구성원을 보여주는 Custom DisclosureGroup
+struct MemberCustomDisclosureGroup: View {
+    @State private var expanded = false
+    @State private var iconViewHeight: CGFloat = 0
+    
+    var body: some View {
         VStack {
             if expanded {
                 VStack {
@@ -167,9 +172,49 @@ struct HeaderView: View {
                 .background(Color.white.opacity(expanded ? 0.5 : 1))
             }
             .padding(.top, -8)
+            
         }
     }
 }
+
+/// 현재위치 버튼
+struct CurrentLocationButton: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Button(action: {
+                    print("현재위치버튼 클릭")
+                }) {
+                    Image(systemName: "location.circle.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.blue)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                }
+                .padding(.bottom, 50)
+                .padding(.leading, 20)
+                Spacer()
+            }
+        }
+    }
+}
+
+/// 해당 화면의 title text
+struct viewTitleText: View {
+    var body: some View {
+        Text("지도")
+            .font(.largeTitle)
+            .bold()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(height: 52)
+        Divider()
+    }
+}
+
+
 
 struct IconView: View {
     var color: Color
