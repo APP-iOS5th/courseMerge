0d2e209e446f8d631c9aad4c455be108137df4d1
DopeKS <kyusang.dev@gmail.com>
2024-06-10 13:45:13 +0900

    MapKkit 설정

--- courseMerge/courseMerge/Views/Map/MapView.swift
+++ courseMerge/courseMerge/Views/Map/MapView.swift
@@ -6,11 +6,18 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    
     var body: some View {
-        VStack {
-            Text("MapView")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea(edges: .all)
         }
     }
 }
