1ea00606ef14b770b8290d0f10169aae1fb86174
DopeKS <kyusang.dev@gmail.com>
2024-06-10 09:31:59 +0900

    Initialize Project coursrMerge

---
+++ courseMerge/courseMerge/courseMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  courseMergeApp.swift
+//  courseMerge
+//
+//  Created by 황규상 on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct courseMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
