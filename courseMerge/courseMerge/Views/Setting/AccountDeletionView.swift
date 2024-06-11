69b3d02c0b0a774a4fedc53f45ccce6e5f3e5f94
Shiene Park <logintolookupsome@gmail.com>
2024-06-11 09:19:57 +0900

    logo

--- courseMerge/courseMerge/Views/Setting/AccountDeletionView.swift
+++ courseMerge/courseMerge/Views/Setting/AccountDeletionView.swift
@@ -13,11 +13,12 @@ struct AccountDeletionView: View {
     
     var body: some View {
         VStack {
-            
-            Image(systemName: "smiley")
+            Image("Logo")
                 .resizable()
-                .scaledToFit()
-                .frame(height: 100)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .padding()
+
             Text("코스머지 탈퇴 전 확인하세요")
             Text("탈퇴하시면 이용 중인 코스머지가 삭제되며, 모든 데이타는 복구가 불가능합니다.")
             ZStack{
@@ -25,13 +26,13 @@ struct AccountDeletionView: View {
                     .foregroundStyle(.gray)
                     .frame(height: 60)
                     .cornerRadius(10)
-                VStack {
-                    Text("모임, 코스, 프로필 등 모든 정보가 삭제됩니다")
-                    Text("구성원과의 채팅은 삭제되지 않으니 미리 확인하세요")
+                VStack(alignment:.leading) {
+                    Text("･ 모임, 코스, 프로필 등 모든 정보가 삭제됩니다")
+                    Text("･ 구성원과의 채팅은 삭제되지 않으니 미리 확인하세요")
                 }
                 
             }
-            Text("blit list")
+            
             Button("안내사항을 모두 확인하였으며, 이에 동의합니다.", systemImage: flag ? "checkmark.circle" : "checkmark.circle.fill") {
                     flag = false
             }
