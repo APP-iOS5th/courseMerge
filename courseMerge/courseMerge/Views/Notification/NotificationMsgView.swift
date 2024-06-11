0ea7ad3735b6bd1a51c6dbe77ba41dc66a158812
Heeji Jung <w030w58@ gmail.com>
2024-06-11 23:44:07 +0900

    1. 프로필뷰 이름 text bool문 추가 2. notification ui 작업 및 구조체 추가 3. memberview viewmodel 연결 (코드 정리x) 4. memberview view끼리 연결 (작업 추가 진행 해야 함..)

---
+++ courseMerge/courseMerge/Views/Notification/NotificationMsgView.swift
@@ -0,0 +1,53 @@
+//
+//  NotificationMessage.swift
+//  CourseMerge
+//
+//  Created by Heeji Jung on 6/11/24.
+//
+
+import SwiftUI
+
+struct NotificationMsgView: View {
+    
+    @EnvironmentObject var userViewModel: UserViewModel
+    @EnvironmentObject var notiViewModel: NotificationViewModel
+   // var testidx: Int  = notiViewModel.notifiMsg.userProfileidx
+    
+    var body: some View {
+        VStack{
+            GeometryReader { geometry in
+                VStack{
+                    ForEach(notiViewModel.notifiMsg){ notimsg in
+                        ZStack{
+                            Rectangle()
+                                .fill(Color.bgSecondary)
+                                .frame(width: geometry.size.width, height: 200) // Rectangle의 너비만큼 설정
+                                .cornerRadius(10)
+                            VStack{
+                                HStack{
+                                    Text(notimsg.partyTitle)
+                                        .fontWeight(.semibold)
+                                        .font(.title)
+                                        .foregroundStyle(.labelsPrimary)
+                                        .padding(.leading)
+                                    ProfileView(user: userViewModel.users[notimsg.userProfileidx], width: 25, height: 25, overlayWidth: 10, overlayHeight: 10, isUsername: false)
+                                    Text(notimsg.datetime)
+                                        .fontWeight(.regular)
+                                        .font(.callout)
+                                        .foregroundStyle(.labelsTertiary)
+                                        .padding(.trailing)
+                                }
+                                Text(notimsg.msg)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .padding(10)
+    }
+}
+
+#Preview {
+    NotificationMsgView()
+}
