6afacf8b214f7754ad1f497450463523bf526dc6
Heeji Jung <w030w58@ gmail.com>
2024-06-12 13:05:40 +0900

    1. 호스트 프로필 뷰에 연결

--- courseMerge/courseMerge/Views/Member/MemberDetailView.swift
+++ courseMerge/courseMerge/Views/Member/MemberDetailView.swift
@@ -69,8 +69,9 @@ struct PartyInfoView: View {
     
     var body: some View{
         HStack {
-            
-            ProfileView(user: userViewModel.users.first!, width: 100, height: 100, overlayWidth: 30, overlayHeight: 50,isUsername: true)
+            ForEach(memberDetailViewModel.createdPartInfo) { partys in
+                ProfileView(user: partys.members.first!, width: 100, height: 100, overlayWidth: 30, overlayHeight: 50,isUsername: true)
+            }
             
             VStack(alignment: .leading) {
                 HStack {
