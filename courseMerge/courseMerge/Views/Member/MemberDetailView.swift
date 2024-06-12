73596b2363ab6587034bfad5af96b1c28e7118bb
Heeji Jung <w030w58@ gmail.com>
2024-06-12 13:20:38 +0900

    누락코드

--- courseMerge/courseMerge/Views/Member/MemberDetailView.swift
+++ courseMerge/courseMerge/Views/Member/MemberDetailView.swift
@@ -70,11 +70,11 @@ struct PartyInfoView: View {
     var body: some View{
         HStack {
             
-            ProfileView(user: userViewModel.users.first!, width: 100, height: 100, overlayWidth: 30, overlayHeight: 50,isUsername: true)
+            //ProfileView(user: userViewModel.users.first!, width: 100, height: 100, overlayWidth: 30, overlayHeight: 50,isUsername: true)
             
-//            ForEach(memberDetailViewModel.createdPartInfo) { partys in
-//                ProfileView(user: partys.members.first!, width: 100, height: 100, overlayWidth: 30, overlayHeight: 50,isUsername: true)
-//            }
+            ForEach(memberDetailViewModel.createdPartInfo) { partys in
+                ProfileView(user: partys.members.first!, width: 100, height: 100, overlayWidth: 30, overlayHeight: 50,isUsername: true)
+            }
             
             VStack(alignment: .leading) {
                 HStack {
